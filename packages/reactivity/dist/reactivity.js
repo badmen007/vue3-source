8347f60684294d9d05b6fd6873903c73c6da7c1b
badmen007 <1572226525@qq.com>
2023-10-09 18:29:25 +0800

    开发环境搭建

---
+++ packages/reactivity/dist/reactivity.js
@@ -0,0 +1,8 @@
+// packages/shared/src/index.ts
+var isObject = (value) => {
+  return value !== null && typeof value === "object";
+};
+export {
+  isObject
+};
+//# sourceMappingURL=reactivity.js.map
---
+++ scripts/dev.js
@@ -0,0 +1,14 @@
+
+const esbuild = require('esbuild')
+const { resolve } = require('path')
+
+const target = 'reactivity'
+
+esbuild.context({
+  entryPoints: [resolve(__dirname, `../packages/${target}/src/index.ts`)],
+  outfile: resolve(__dirname, `../packages/${target}/dist/${target}.js`),
+  bundle: true, // 将依赖的恶模块全部打包
+  sourcemap: true,
+  format: "esm",
+  platform: "browser",
+}).then(ctx => ctx.watch());
